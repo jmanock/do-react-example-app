d7aa195f6b0ebe86913d260d8b0871ac66842f6c
Jon Manock <jonmanock@Jons-MBP.lan>
2018-11-30 16:57:32 -0500

    working on the new cal

--- src/components/calendarKnew.jsx
+++ src/components/calendarKnew.jsx
@@ -78,6 +78,92 @@ class Calendar extends Compnent{
       weeks.push(
         <Week previousCurrentNextView={previousCurrentNextView.clone()} currentMonthView={currentMonthView} monthEvents={monthEvents} selected={currentSelectedDay} select={day => this.select(day)} />
       );
+      previousCurrentNextView.add(1,'w');
+      done = count++ > 2 && monthIndex !== previousCurrentNextView.month();
+    }
+    return weeks;
+  }
+
+  handleAdd(){
+    const monthEvents = this.state.selectedMonthEvents;
+    const currentEvents = this.state.selectedMonthEvents;
+    const currentSelectedDate = this.state.selectedDay;
+
+    let newEvents = [];
+    var eventTitle = prompt('Please enter a name for your event:');
+
+    switch(eventTitle){
+      case '':
+        alert('Event name cannot be empty');
+        break;
+      case null:
+        alert('Changed your mind? You can add one later!');
+        break;
+      default:
+        var newEvent = {
+          title:eventTitle,
+          date:currentSelectedDate,
+          dynamic:true
+        };
+
+        newEvents.push(newEvent);
+        for(var i = 0; i < newEvents.length; i++){
+          monthEvents.push(newEvents[i]);
+        }
+
+        this.setState({
+          selectedMonthEvents:monthEvents
+        });
+        break;
+    }
+  }
+
+  addEvent(){
+    const currentSelectedDate = this.state.selectedDay;
+    let isAfterDay = moment().startOf('day').subtract(1, 'd');
+
+    if(currentSelectedDate.isAfter(isAfterDay)){
+      this.handleAdd();
+    }else{
+      if(confirm('are you sure you want to add an event in the post?')){
+        this.handleAdd();
+      }else{
+
+      }
+    }
+  }
+
+  removeEvent(i){
+    const monthEvents = this.state.selectedMonthEvents.slice();
+    const currentSelectedDate = this.state.selectedDay;
+
+    if(confirm('Are you sure you want to remove this event?')){
+      let index = i;
+      if(index != -1){
+        monthEvents.splice(index,1);
+      } else{
+        alert('no events to remove on this day');
+      }
+      this.setState({
+        selectedMonthEvents:monthEvents
+      });
+    }
+  }
+
+  initialiseEvents(){
+    const monthEvents = this.state.selectedMonthEvents;
+    let allEvents = [];
+    var event1 = {
+      title:'Something Something something dark side',
+      date:moment(),
+      dynamic:false
+    };
+    var event2 = {
+      title:'somethinElse something else something else',
+      date:moment().startOf('day').subtract(2,'d').add(2, 'h'),
+      dynamic:false
+    };
+    var event3 = {
       
     }
   }
